6574be0a84dd60be283383f3bf322042d3517509
Christoph Klemtz <christoph.klemtz@dci-student.org>
2025-05-08 19:48:35 +0200

    fixed db.js - connection warnings

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    // Die Optionen useNewUrlParser und useUnifiedTopology sind nicht mehr nötig
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
